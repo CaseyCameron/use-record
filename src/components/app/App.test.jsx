e724d9da05ae5793d8a80ea865885688356ccd7d
Casey Cameron <mcasey.cameron@gmail.com>
2021-08-16 16:38:49 -0700

    Tests passing

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,59 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { cleanup, fireEvent, render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
   afterEach(() => cleanup());
   it('renders App', () => {
-    const { asFragment } = render(<App />);
-    expect(asFragment()).toMatchSnapshot();
+    render(<App />);
+
+    const colorDisplay = screen.getByTestId('color-display');
+    expect(colorDisplay).toHaveStyle({ backgroundColor: '#FF0000' });
+
+  });
+
+  it('Changes Color', () => {
+    render(<App />);
+    const colorInput = screen.getByTestId('color-input');
+    const colorDisplay = screen.getByTestId('color-display');
+
+    fireEvent.change(colorInput, { target: { value: '#000000' } });
+
+    expect(colorDisplay).toHaveStyle({ backgroundColor: '#000000' });
+  });
+
+  it('Undo', () => {
+    render(<App />);
+    const colorInput = screen.getByTestId('color-input');
+    const colorDisplay = screen.getByTestId('color-display');
+    const undo = screen.getByRole('button', { name: 'undo' });
+
+    fireEvent.change(colorInput, { target: { value: '#000000' } });
+    fireEvent.change(colorInput, { target: { value: '#FFFFFF' } });
+    fireEvent.change(colorInput, { target: { value: '#FFFF00' } });
+
+    fireEvent.click(undo);
+    expect(colorDisplay).toHaveStyle({ backgroundColor: '#FFFFFF' });
+
+    fireEvent.click(undo);
+    expect(colorDisplay).toHaveStyle({ backgroundColor: '#000000' });
+  });
+
+  it('Redo', () => {
+    render(<App />);
+    const colorInput = screen.getByTestId('color-input');
+    const colorDisplay = screen.getByTestId('color-display');
+    const undo = screen.getByRole('button', { name: 'undo' });
+    const redo = screen.getByRole('button', { name: 'redo' });
+
+    fireEvent.change(colorInput, { target: { value: '#000000' } });
+    fireEvent.change(colorInput, { target: { value: '#FFFFFF' } });
+    fireEvent.change(colorInput, { target: { value: '#FFFF00' } });
+
+    fireEvent.click(undo);
+    expect(colorDisplay).toHaveStyle({ backgroundColor: '#FFFFFF' });
+
+    fireEvent.click(redo);
+    expect(colorDisplay).toHaveStyle({ backgroundColor: '#FFF00' });
   });
 });
